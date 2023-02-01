e23066463b320c88993f5f6dd3204c2fa1a4642b
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-02-01 09:28:57 +0700

    Handle DB errors: change internalServerError to Forbidden when violates constraint

--- api/account.go
+++ api/account.go
@@ -3,8 +3,8 @@ package api
 import (
 	"database/sql"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "github.com/techschool/simplebank/db/sqlc"
 )
 
@@ -13,7 +13,7 @@ type CreateAccountRequest struct {
 	Currency string `json:"currency" binding:"required",currency`
 }
 
-func (server *Server) CreateAccount(ctx *gin.Context){
+func (server *Server) CreateAccount(ctx *gin.Context) {
 	var req CreateAccountRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
@@ -21,13 +21,20 @@ func (server *Server) CreateAccount(ctx *gin.Context){
 	}
 
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
-		Balance: 0,
+		Owner:    req.Owner,
+		Balance:  0,
 		Currency: req.Currency,
 	}
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if postgresErr, ok := err.(*pq.Error); ok {
+			switch postgresErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
@@ -39,7 +46,7 @@ type GetAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
-func (server *Server) GetAccount(ctx *gin.Context){
+func (server *Server) GetAccount(ctx *gin.Context) {
 	var req GetAccountRequest
 	if err := ctx.ShouldBindUri(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
@@ -60,11 +67,11 @@ func (server *Server) GetAccount(ctx *gin.Context){
 }
 
 type ListAccountsRequest struct {
-	PageID int32 `form:"page_id" binding:"required,min=1"`
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
-func (server *Server) ListAccounts(ctx *gin.Context){
+func (server *Server) ListAccounts(ctx *gin.Context) {
 	var req ListAccountsRequest
 	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
@@ -72,8 +79,8 @@ func (server *Server) ListAccounts(ctx *gin.Context){
 	}
 
 	arg := db.ListAccountsParams{
-		Limit: req.PageSize,
-		Offset: (req.PageID - 1)*req.PageSize,
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
 	}
 	accounts, err := server.store.ListAccounts(ctx, arg)
 	if err != nil {
