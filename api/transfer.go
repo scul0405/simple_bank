35e14d95ed56bdaa522c33fbf332ee1da29334cd
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-02-03 14:50:03 +0700

    Implement auth middleware for transfer

--- api/transfer.go
+++ api/transfer.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/token"
 )
 
 type CreateTransferRequest struct {
@@ -29,11 +31,21 @@ func (server *Server) CreateTransfer(ctx *gin.Context){
 		Amount: req.Amount,	
 	}
 
-	if !server.IsValidAccount(ctx, req.FromAccountID, req.Currency){
+	fromAccount, valid := server.IsValidAccount(ctx, req.FromAccountID, req.Currency)
+
+	if !valid{
 		return
 	}
 
-	if !server.IsValidAccount(ctx, req.ToAccountID, req.Currency){
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	_, valid = server.IsValidAccount(ctx, req.ToAccountID, req.Currency)
+	if !valid{
 		return
 	}
 
@@ -47,22 +59,22 @@ func (server *Server) CreateTransfer(ctx *gin.Context){
 }
 
 // IsValidAccount check if account is exist or account's currency is match with transfer's currency
-func (server *Server) IsValidAccount(ctx *gin.Context, accountID int64, currency string) bool {
+func (server *Server) IsValidAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
 	}
 
 	if account.Currency != currency {
 		err := fmt.Errorf("account with ID '%d' currency mismatch: %s vs %s", accountID, account.Currency, currency)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return false
+		return account, false
 	}
 
-	return true
+	return account, true
 }
